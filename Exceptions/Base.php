9261f9c07ccdacee3fdd37addf07a8ad1d026139
demorfi <demorfi@gmail.com>
2022-11-22 22:28:04 +0700

    added: register event for base exception

--- Exceptions/Base.php
+++ Exceptions/Base.php
@@ -3,8 +3,14 @@
 namespace Digua\Exceptions;
 
 use Exception;
+use Throwable;
+use Digua\Event;
 
 class Base extends Exception
 {
-
+    public function __construct(string $message = '', int $code = 0, ?Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        Event::register([__CLASS__, get_called_class()], $this);
+    }
 }
