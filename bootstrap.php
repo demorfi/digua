60e22135df60f7547f0b452be6da6b8414dcd58c
demorfi <demorfi@gmail.com>
2023-02-19 13:22:58 +0700

    added: bootstrap.php

---
+++ bootstrap.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+if (!defined('ROOT_PATH')) {
+    throw new Exception('Root path is not defined!');
+}
+
+$loader = require_once __DIR__ . '/autoload.php';
+$loader->addIncludePath(ROOT_PATH . '/../$1', __DIR__ . '/../$1/src');
+
+Digua\Env::dev();
+Digua\Env::addHandlerError();
+Digua\Env::addHandlerException();
+
+return $loader;
