805e2b7f25fe1b6edbcb3428a2b24c49daac34c2
demorfi <demorfi@gmail.com>
2023-03-01 18:51:55 +0700

    added: file component

---
+++ src/Components/File.php
@@ -0,0 +1,141 @@
+<?php declare(strict_types=1);
+
+namespace Digua\Components;
+
+use Digua\Exceptions\File as FileException;
+
+class File
+{
+    /**
+     * @var mixed|false|resource
+     */
+    protected readonly mixed $handle;
+
+    /**
+     * @param string $filePath
+     * @throws FileException
+     */
+    public function __construct(private readonly string $filePath)
+    {
+        if (is_dir($this->filePath) || !is_writable($this->filePath)) {
+            throw new FileException($this->filePath . ' - file is not writable!');
+        }
+
+        $this->handle = fopen($this->filePath, 'r+');
+    }
+
+    public function __destruct()
+    {
+        fclose($this->handle);
+    }
+
+    /**
+     * @return bool
+     * @throws FileException
+     */
+    public function readLock(): bool
+    {
+        if (!flock($this->handle, LOCK_SH)) {
+            throw new FileException($this->filePath . ' - failed to lock file!');
+        }
+        return true;
+    }
+
+    /**
+     * @return bool
+     * @throws FileException
+     */
+    public function writeLock(): bool
+    {
+        if (!flock($this->handle, LOCK_EX)) {
+            throw new FileException($this->filePath . ' - failed to lock file!');
+        }
+        return true;
+    }
+
+    /**
+     * @return bool
+     * @throws FileException
+     */
+    public function unlock(): bool
+    {
+        if (!flock($this->handle, LOCK_UN)) {
+            throw new FileException($this->filePath . ' - failed to unlock file!');
+        }
+        return true;
+    }
+
+    /**
+     * @return int
+     */
+    public function filesize(): int
+    {
+        fseek($this->handle, 0, SEEK_END);
+        return (int)ftell($this->handle);
+    }
+
+    /**
+     * @param int $size
+     * @return bool
+     */
+    public function truncate(int $size): bool
+    {
+        return ftruncate($this->handle, $size);
+    }
+
+    /**
+     * @return bool
+     */
+    public function empty(): bool
+    {
+        return $this->truncate(0);
+    }
+
+    /**
+     * @param int      $offset
+     * @param int|null $length
+     * @return string
+     */
+    public function readLeft(int $offset = 0, ?int $length = null): string
+    {
+        $filesize = $this->filesize();
+        fseek($this->handle, abs($offset));
+        return (string)fread($this->handle, $length ?: $filesize ?: 1);
+    }
+
+    /**
+     * @param int      $offset
+     * @param int|null $length
+     * @return string
+     */
+    public function readRight(int $offset = 0, ?int $length = null): string
+    {
+        $filesize = $this->filesize();
+        fseek($this->handle, -abs($offset) ?: -$length ?: -$filesize, SEEK_END);
+        return (string)fread($this->handle, $length ?: $filesize ?: 1);
+    }
+
+    /**
+     * @param string   $data
+     * @param int      $offset
+     * @param int|null $length
+     * @return bool
+     */
+    public function writeLeft(string $data, int $offset = 0, ?int $length = null): bool
+    {
+        fseek($this->handle, abs($offset));
+        return (bool)fwrite($this->handle, $data, $length);
+    }
+
+    /**
+     * @param string   $data
+     * @param int      $offset
+     * @param int|null $length
+     * @return bool
+     */
+    public function writeRight(string $data, int $offset = 0, ?int $length = null): bool
+    {
+        fseek($this->handle, -abs($offset), SEEK_END);
+        return (bool)fwrite($this->handle, $data, $length);
+    }
+}
---
+++ tests/Components/FileTest.php
@@ -0,0 +1,75 @@
+<?php declare(strict_types=1);
+
+namespace Components;
+
+use Digua\Components\File;
+use Digua\Exceptions\File as FileException;
+use PHPUnit\Framework\TestCase;
+
+class FileTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        file_put_contents(__DIR__ . '/file-test.txt', 'file test content');
+    }
+
+    /**
+     * @return void
+     */
+    protected function tearDown(): void
+    {
+        unlink(__DIR__ . '/file-test.txt');
+    }
+
+    /**
+     * @return void
+     * @throws FileException
+     */
+    public function testReadFile(): void
+    {
+        $file = new File(__DIR__ . '/file-test.txt');
+        $this->assertTrue($file->readLock());
+
+        $this->assertEquals('file test content', $file->readLeft());
+        $this->assertEquals('fi', $file->readLeft(0, 2));
+        $this->assertEquals('le te', $file->readLeft(2, 5));
+        $this->assertEquals('le test content', $file->readLeft(2));
+
+        $this->assertEquals('file test content', $file->readRight());
+        $this->assertEquals('content', $file->readRight(7));
+        $this->assertEquals('n', $file->readRight(2, 1));
+        $this->assertEquals('content', $file->readRight(0, 7));
+        $this->assertTrue($file->unlock());
+    }
+
+    /**
+     * @return void
+     * @throws FileException
+     */
+    public function testWriteFile(): void
+    {
+        $file = new File(__DIR__ . '/file-test.txt');
+        $this->assertTrue($file->writeLock());
+
+        $this->assertTrue($file->writeLeft('1', 0, 1));
+        $this->assertEquals('1ile test content', $file->readLeft());
+        $this->assertTrue($file->writeLeft('2', 2, 1));
+        $this->assertEquals('1i2e test content', $file->readLeft());
+
+        $this->assertTrue($file->writeRight('1'));
+        $this->assertEquals('1i2e test content1', $file->readRight());
+        $this->assertTrue($file->writeRight('2', 2, 1));
+        $this->assertEquals('1i2e test conten21', $file->readRight());
+
+        $this->assertTrue($file->truncate(2));
+        $this->assertEquals('1i', $file->readLeft());
+
+        $this->assertTrue($file->empty());
+        $this->assertEquals('', $file->readLeft());
+
+        $this->assertTrue($file->unlock());
+    }
+}
