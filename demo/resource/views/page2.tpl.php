574ec45760b7e90ed5939ca3a08a486050cca202
demorfi <demorfi@gmail.com>
2023-03-03 07:57:13 +0700

    removed: demo from main branch

--- demo/resource/views/page2.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php
-$this->extend('layout');
-$this->shortSection('header', 'parts/_menu');
-?>
-
-<?php $this->section('content'); ?>
-<div>It's Works!</div>
-<?php $this->endSection('content'); ?>
--- demo/web/index.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-define('ROOT_PATH', realpath(__DIR__ . '/..'));
-
-require_once realpath(ROOT_PATH . '/../bootstrap.php');
-require_once ROOT_PATH . '/app/helpers.php';
-
-Digua\Env::prod();
-print (new Digua\RouteDispatcher())->default();
