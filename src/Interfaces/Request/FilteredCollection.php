128b9b29bbb84456eb85872963cf245fae93040a
demorfi <demorfi@gmail.com>
2023-04-18 10:55:28 +0700

    added: Request -> FilteredCollection interface

---
+++ src/Interfaces/Request/FilteredCollection.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Digua\Interfaces\Request;
+
+use Digua\Interfaces\NamedCollection as NamedCollectionInterface;
+
+interface FilteredCollection extends NamedCollectionInterface
+{
+    /**
+     * @return FilteredInput
+     */
+    public function filtered(): FilteredInput;
+}
