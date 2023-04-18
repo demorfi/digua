8ffb207c9b800a193cbe7ddcc0f6efe367237a2f
demorfi <demorfi@gmail.com>
2023-04-18 10:56:58 +0700

    added: test Request -> Data

---
+++ tests/Request/DataTest.php
@@ -0,0 +1,46 @@
+<?php declare(strict_types=1);
+
+namespace Request;
+
+use PHPUnit\Framework\TestCase;
+use Digua\Request\{Data, Cookies, Post, Query};
+
+class DataTest extends TestCase
+{
+    /**
+     * @var Data
+     */
+    private Data $data;
+
+    /**
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        $this->data = new Data;
+    }
+
+    /**
+     * @return void
+     */
+    public function testPostObjectIsReturned(): void
+    {
+        $this->assertInstanceOf(Post::class, $this->data->post());
+    }
+
+    /**
+     * @return void
+     */
+    public function testQueryObjectIsReturned(): void
+    {
+        $this->assertInstanceOf(Query::class, $this->data->query());
+    }
+
+    /**
+     * @return void
+     */
+    public function testCookiesObjectIsReturned(): void
+    {
+        $this->assertInstanceOf(Cookies::class, $this->data->cookies());
+    }
+}
