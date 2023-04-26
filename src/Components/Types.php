6384ab10db985b7ea0756b726e0a98832444e4bd
demorfi <demorfi@gmail.com>
2023-04-26 07:33:44 +0700

    added: component Types

---
+++ src/Components/Types.php
@@ -0,0 +1,100 @@
+<?php declare(strict_types=1);
+
+namespace Digua\Components;
+
+class Types
+{
+    const ALIAS_TYPES = ['boolean' => 'bool', 'integer' => 'int'];
+
+    /**
+     * @var string
+     */
+    protected readonly string $short;
+
+    /**
+     * @var string
+     */
+    protected readonly string $long;
+
+    /**
+     * @param mixed $value
+     */
+    public function __construct(protected readonly mixed $value)
+    {
+        $type = strtolower(gettype($this->value));
+
+        $this->long  = $type === 'double' ? 'float' : $type;
+        $this->short = self::ALIAS_TYPES[$this->long] ?? $this->long;
+    }
+
+    /**
+     * @param mixed $value
+     * @return static
+     */
+    public static function value(mixed $value): static
+    {
+        return new static($value);
+    }
+
+    /**
+     * @param string $type
+     * @return static
+     */
+    public static function type(string $type): static
+    {
+        settype($type, $type);
+        return new static($type);
+    }
+
+    /**
+     * @param string $type
+     * @return static
+     */
+    public function to(string $type): static
+    {
+        $value = $this->value;
+        settype($value, $type);
+        return new static($value);
+    }
+
+    /**
+     * @return string
+     */
+    public function getNameShort(): string
+    {
+        return $this->short;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNameLong(): string
+    {
+        return $this->long;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getValue(): mixed
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param string $type
+     * @return bool
+     */
+    public function is(string $type): bool
+    {
+        return $this->short === $type || $this->long === $type || $this->value instanceof $type;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isNull(): bool
+    {
+        return $this->short === 'null';
+    }
+}
---
+++ tests/Components/TypesTest.php
@@ -0,0 +1,127 @@
+<?php declare(strict_types=1);
+
+namespace Tests\Components;
+
+use Digua\Components\Types;
+use PHPUnit\Framework\TestCase;
+
+class TypesTest extends TestCase
+{
+    const TYPES = ['bool', 'boolean', 'int', 'integer', 'double', 'float', 'string', 'array', 'object', 'null'];
+
+    /**
+     * @return array[]
+     */
+    protected function dataTypesProvider(): array
+    {
+        return [
+            'null'   => ['null', 'null', null],
+            'bool'   => ['bool', 'boolean', true, false],
+            'int'    => ['int', 'integer', PHP_INT_MIN, PHP_INT_MAX],
+            'float'  => ['float', 'float', PHP_FLOAT_MIN, PHP_FLOAT_MAX],
+            'string' => ['string', 'string', 'test string'],
+            'array'  => ['array', 'array', []],
+            'object' => ['object', 'object', $this]
+        ];
+    }
+
+    /**
+     * @dataProvider dataTypesProvider
+     * @param string $shortName
+     * @param string $longName
+     * @param mixed  ...$values
+     * @return void
+     */
+    public function testNameOfValue(string $shortName, string $longName, mixed ...$values): void
+    {
+        foreach ($values as $value) {
+            $this->assertSame($shortName, Types::value($value)->getNameShort());
+            $this->assertSame($longName, Types::value($value)->getNameLong());
+        }
+    }
+
+    /**
+     * @dataProvider dataTypesProvider
+     * @param string $shortName
+     * @param string $longName
+     * @param mixed  ...$values
+     * @return void
+     */
+    public function testIsItPossibleToReturnValue(string $shortName, string $longName, mixed ...$values): void
+    {
+        foreach ($values as $value) {
+            $this->assertSame($value, Types::value($value)->getValue());
+        }
+    }
+
+    /**
+     * @dataProvider dataTypesProvider
+     * @param string $shortName
+     * @param string $longName
+     * @return void
+     */
+    public function testNameOfType(string $shortName, string $longName): void
+    {
+        $this->assertSame($shortName, Types::type($shortName)->getNameShort());
+        $this->assertSame($shortName, Types::type($longName)->getNameShort());
+
+        $this->assertSame($longName, Types::type($shortName)->getNameLong());
+        $this->assertSame($longName, Types::type($longName)->getNameLong());
+    }
+
+    /**
+     * @dataProvider dataTypesProvider
+     * @param string $shortName
+     * @param string $longName
+     * @param mixed  ...$values
+     * @return void
+     */
+    public function testIsType(string $shortName, string $longName, mixed ...$values): void
+    {
+        foreach ($values as $value) {
+            $this->assertTrue(Types::value($value)->is($shortName));
+            $this->assertTrue(Types::value($value)->is($longName));
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function testIsObjectTypeOfClassName(): void
+    {
+        $this->assertTrue(Types::value($this)->is($this::class));
+    }
+
+    /**
+     * @return void
+     */
+    public function testIsNullType(): void
+    {
+        $this->assertTrue(Types::value(null)->isNull());
+        $this->assertFalse(Types::value(false)->isNull());
+    }
+
+    /**
+     * @dataProvider dataTypesProvider
+     * @param string $shortName
+     * @param string $longName
+     * @param mixed  ...$values
+     * @return void
+     */
+    public function testConvertType(string $shortName, string $longName, mixed ...$values): void
+    {
+        $exceptions = ['array' => ['string'], 'object' => ['int', 'float', 'string']];
+        foreach ($values as $value) {
+            $tValue = Types::value($value);
+            foreach (self::TYPES as $type) {
+                $tType     = Types::type($type);
+                $exception = $exceptions[$tValue->getNameShort()] ?? false;
+                if (is_array($exception) && in_array($tType->getNameShort(), $exception)) {
+                    continue;
+                }
+
+                $this->assertSame($tType->getNameLong(), $tValue->to($type)->getNameLong());
+            }
+        }
+    }
+}
