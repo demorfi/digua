2334dd83c1ff57ff762a5a628453adc3d8b02b82
demorfi <demorfi@gmail.com>
2023-04-30 11:36:20 +0700

    added: method {filter} in ArrayCollection added: method {collection} of Data trait returned ArrayCollection added: method {getValues} in Data trait changed: ArrayCollection implements {NamedCollection, Countable, ArrayAccess, IteratorAggregate, JsonSerializable} interfaces changed: methods {only, slice} of Data trait moved to ArrayCollection changed: method {shake} of NamedCollection interface moved to FilteredCollection interface

--- src/Components/ArrayCollection.php
+++ src/Components/ArrayCollection.php
@@ -3,15 +3,22 @@
 namespace Digua\Components;
 
 use Digua\Traits\Data;
+use Digua\Interfaces\NamedCollection;
+use IteratorAggregate;
+use Traversable;
+use ArrayAccess;
+use ArrayIterator;
+use JsonSerializable;
+use Countable;
 
-class ArrayCollection
+class ArrayCollection implements NamedCollection, Countable, ArrayAccess, IteratorAggregate, JsonSerializable
 {
     use Data;
 
     /**
      * @param array $values
      */
-    public function __construct(array $values)
+    public function __construct(array $values = [])
     {
         $this->array = $values;
     }
@@ -20,8 +27,119 @@ class ArrayCollection
      * @param array $values
      * @return static
      */
-    public static function make(array $values): static
+    public static function make(array $values = []): static
     {
         return new static($values);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function count(): int
+    {
+        return $this->size();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function offsetSet(mixed $offset, mixed $value): void
+    {
+        if (is_null($offset)) {
+            $this->array[] = $value;
+        } else {
+            $this->array[$offset] = $value;
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function offsetExists(mixed $offset): bool
+    {
+        return $this->__isset($offset);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function offsetUnset(mixed $offset): void
+    {
+        $this->__unset($offset);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function offsetGet(mixed $offset): mixed
+    {
+        return $this->__get($offset);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function jsonSerialize(): array
+    {
+        return $this->array;
+    }
+
+    /**
+     * @return Traversable
+     */
+    public function getIterator(): Traversable
+    {
+        return new ArrayIterator($this->array);
+    }
+
+    /**
+     * @return array
+     */
+    public function toArray(): array
+    {
+        return $this->array;
+    }
+
+    /**
+     * @param string ...$names
+     * @return static
+     */
+    public function only(string ...$names): static
+    {
+        $array = [];
+        foreach ($names as $name) {
+            $array[$name] = $this->array[$name] ?? null;
+        }
+
+        return static::make($array);
+    }
+
+    /**
+     * Slice array by key.
+     *
+     * @param string    $prefix
+     * @param ?callable $callable
+     * @return static
+     */
+    public function slice(string $prefix, callable $callable = null): static
+    {
+        $array = [];
+        foreach ($this->array as $key => $value) {
+            if (($pos = stripos($key, $prefix)) !== false && (is_null($callable) || $callable($key, $value))) {
+                $array[substr($key, ($pos + strlen($prefix)))] = $value;
+            }
+        }
+
+        return static::make($array);
+    }
+
+    /**
+     * @param ?callable $callable
+     * @param int       $mode
+     * @return static
+     */
+    public function filter(?callable $callable = null, int $mode = ARRAY_FILTER_USE_BOTH): static
+    {
+        return static::make(array_filter($this->array, $callable, $mode));
     }
 }
