fd7099ec2d46ad95626cb2cf438045c73650efaa
demorfi <demorfi@gmail.com>
2023-05-11 00:45:58 +0700

    changed: Error controller defines the error code and message by request->abort exception

--- src/Controllers/Error.php
+++ src/Controllers/Error.php
@@ -2,17 +2,49 @@
 
 namespace Digua\Controllers;
 
-use Digua\Interfaces\Route;
 use Digua\Response;
-use Digua\Exceptions\Path as PathException;
-use Digua\Interfaces\Guardian as GuardianInterface;
+use Digua\Enums\Headers;
+use Digua\Exceptions\{
+    Path as PathException,
+    Abort as AbortException
+};
+use Digua\Interfaces\{
+    Request as RequestInterface,
+    Route as RouteInterface,
+    Guardian as GuardianInterface
+};
 
 class Error extends Base implements GuardianInterface
 {
     /**
+     * @var int
+     */
+    protected int $code = 404;
+
+    /**
+     * @var string
+     */
+    protected string $message;
+
+    /**
      * @inheritdoc
      */
-    public function granted(Route $route): bool
+    public function __construct(protected RequestInterface $request)
+    {
+        parent::__construct($this->request);
+        $this->message = Headers::from($this->code)->getText();
+
+        $exception = $this->request->getException();
+        if ($exception instanceof AbortException) {
+            $this->code    = $exception->getCode();
+            $this->message = $exception->getMessage() ?: Headers::from($this->code)->getText();
+        }
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function granted(RouteInterface $route): bool
     {
         return true;
     }
@@ -24,7 +56,10 @@ class Error extends Base implements GuardianInterface
     public function defaultAction(): Response|array
     {
         $isAsync = $this->request->getData()->query()->isAsync();
-        return Response::create($isAsync ? ['error' => 'not found'] : $this->render('error'))
-            ->addHeader('http', 'HTTP/1.1 404 Not Found', 404);
+        return Response::create(
+            $isAsync
+                ? ['error' => $this->message]
+                : $this->render('error', ['code' => $this->code, 'message' => $this->message])
+        )->addHttpHeader(Headers::from($this->code));
     }
 }
