71e2c9dd0434e14e67a2f6957700f4eac366176f
demorfi <demorfi@gmail.com>
2023-09-25 08:29:10 +0700

    added: DotEnv component

---
+++ src/Components/DotEnv.php
@@ -0,0 +1,36 @@
+<?php declare(strict_types=1);
+
+namespace Digua\Components;
+
+use Digua\Exceptions\File as FileException;
+
+class DotEnv
+{
+    /**
+     * @param string $filePath
+     * @throws FileException
+     */
+    public function __construct(protected string $filePath)
+    {
+        if (!is_file($this->filePath)) {
+            throw new FileException(sprintf('file %s does not exist!', $this->filePath));
+        }
+    }
+
+    /**
+     * @return void
+     * @throws FileException
+     */
+    public function load(): void
+    {
+        if (!is_readable($this->filePath)) {
+            throw new FileException(sprintf('file %s is unreadable!', $this->filePath));
+        }
+
+        if (!empty($envData = parse_ini_file($this->filePath))) {
+            foreach ($envData as $envKey => $envValue) {
+                putenv(sprintf('%s=%s', $envKey, $envValue));
+            }
+        }
+    }
+}
---
+++ tests/Components/DotEnvTest.php
@@ -0,0 +1,80 @@
+<?php declare(strict_types=1);
+
+namespace Digua\Components;
+
+/**
+ * Redefined function for testing load method.
+ *
+ * @param string $filePath
+ * @return bool
+ */
+function is_readable(string $filePath): bool
+{
+    return !str_contains($filePath, 'unreadable') && is_file($filePath);
+}
+
+namespace Tests\Components;
+
+use Digua\Components\DotEnv;
+use Digua\Exceptions\File as FileException;
+use PHPUnit\Framework\TestCase;
+
+class DotEnvTest extends TestCase
+{
+    /**
+     * @return void
+     */
+    protected function setUp(): void
+    {
+        file_put_contents(__DIR__ . '/.env-readable', 'ENV_KEY=ENV_VALUE');
+        file_put_contents(__DIR__ . '/.env-unreadable', 'ENV_KEY=ENV_VALUE');
+    }
+
+    /**
+     * @return void
+     */
+    protected function tearDown(): void
+    {
+        unlink(__DIR__ . '/.env-readable');
+        unlink(__DIR__ . '/.env-unreadable');
+    }
+
+    /**
+     * @return void
+     * @throws FileException
+     */
+    public function testFileNotFound(): void
+    {
+        $filePath = __DIR__ . '/.env-not-found';
+        $this->expectException(FileException::class);
+        $this->expectExceptionMessage('file ' . $filePath . ' does not exist!');
+        new DotEnv($filePath);
+    }
+
+    /**
+     * @return void
+     * @throws FileException
+     */
+    public function testFileNotReadable(): void
+    {
+        $filePath = __DIR__ . '/.env-unreadable';
+        $this->expectException(FileException::class);
+        $this->expectExceptionMessage('file ' . $filePath . ' is unreadable!');
+
+        // function is_readable redefined!
+        (new DotEnv($filePath))->load();
+    }
+
+    /**
+     * @return void
+     * @throws FileException
+     */
+    public function testLoadAndParsingFile(): void
+    {
+        $this->assertFalse(getenv('ENV_KEY'));
+
+        // function is_readable redefined!
+        (new DotEnv(__DIR__ . '/.env-readable'))->load();
+        $this->assertSame('ENV_VALUE', getenv('ENV_KEY'));
+    }
+}
