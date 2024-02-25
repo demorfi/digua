98d802c8f964925b6e3466b2012286083d21691c
demorfi <demorfi@gmail.com>
2024-02-25 14:41:34 +0700

    changed: exception Injector may return an erroneous parameter via the method getParameter

--- src/Exceptions/Injector.php
+++ src/Exceptions/Injector.php
@@ -2,7 +2,33 @@
 
 namespace Digua\Exceptions;
 
+use ReflectionParameter;
+use Throwable;
+
 class Injector extends Base
 {
+    /**
+     * @var ReflectionParameter
+     */
+    protected ReflectionParameter $parameter;
 
+    /**
+     * @param ReflectionParameter $parameter
+     * @param int                 $code
+     * @param string              $message
+     * @param ?Throwable          $previous
+     */
+    public function __construct(ReflectionParameter $parameter, string $message = '', int $code = 0, ?Throwable $previous = null)
+    {
+        $this->parameter = $parameter;
+        parent::__construct($message, $code, $previous);
+    }
+
+    /**
+     * @return ReflectionParameter
+     */
+    public function getParameter(): ReflectionParameter
+    {
+        return $this->parameter;
+    }
 }
